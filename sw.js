4045bf55ba778c528e3d8be04ca461bb88a816af
xuyikai1 <xuyikai123@qq.com>
2020-06-23 19:35:28 +0800

    Site updated: 2020-06-23 19:35:23

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/6.html');
+  precacheUrls.push('/posts/6828.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
