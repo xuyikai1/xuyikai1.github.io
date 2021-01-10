73a7b5b7bcc8adaa6295dbcf8dc6438864fd0304
xuyikai1 <xuyikai123@qq.com>
2021-01-10 11:54:12 +0800

    Site updated: 2021-01-10 11:53:47

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
+  precacheUrls.push('/posts/mysql-configure.html');
+
   precacheUrls.push('/posts/aop.html');
 
   precacheUrls.push('/posts/redisson.html');
 
   precacheUrls.push('/posts/HashMap.html');
-
-  precacheUrls.push('/posts/mysql-configure.html');
 
   precacheUrls.push('/posts/rabbitmq.html');
 
@@ -38,15 +38,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/ribbon-load-balance.html');
 
-  precacheUrls.push('/posts/SpringBoot-autoConfiguration.html');
+  precacheUrls.push('/posts/threadPool.html');
 
   precacheUrls.push('/posts/shutdownHook.html');
 
+  precacheUrls.push('/posts/SpringBoot-autoConfiguration.html');
+
   precacheUrls.push('/posts/spring-event.html');
 
-  precacheUrls.push('/posts/xxl-job.html');
-
-  precacheUrls.push('/posts/threadPool.html');
+  precacheUrls.push('/posts/spring-annotation.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
