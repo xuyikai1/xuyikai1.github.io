268b9e3c2e3c655eeeeea2fcabc78ec489e6af7e
xuyikai1 <xuyikai123@qq.com>
2021-02-07 11:33:17 +0800

    Site updated: 2021-02-07 11:32:50

--- sw.js
+++ sw.js
@@ -24,25 +24,25 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/aop.html');
-
   precacheUrls.push('/posts/interview-jvm-classloader.html');
 
-  precacheUrls.push('/posts/mysql-configure.html');
+  precacheUrls.push('/posts/interview-MySQL-common.html');
 
-  precacheUrls.push('/posts/interview-jvm-GarbageCollection.html');
+  precacheUrls.push('/posts/aop.html');
 
   precacheUrls.push('/posts/interview-jvm-model.html');
 
+  precacheUrls.push('/posts/interview-jvm-GarbageCollection.html');
+
   precacheUrls.push('/posts/hmap.html');
 
-  precacheUrls.push('/posts/redisson.html');
+  precacheUrls.push('/posts/mysql-configure.html');
 
   precacheUrls.push('/posts/rabbitmq.html');
 
-  precacheUrls.push('/posts/redis-cluster.html');
+  precacheUrls.push('/posts/redisson.html');
 
-  precacheUrls.push('/posts/ribbon-load-balance.html');
+  precacheUrls.push('/posts/redis-cluster.html');
 
   precacheUrls.push('/posts/shutdownHook.html');
 
