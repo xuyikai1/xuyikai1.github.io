6fd2f5b7c6f53678de44ddd398982bdd08254084
xuyikai1 <xuyikai123@qq.com>
2021-02-24 16:37:06 +0800

    Site updated: 2021-02-24 16:36:42

--- sw.js
+++ sw.js
@@ -26,19 +26,19 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/aop.html');
 
-  precacheUrls.push('/posts/interview-jvm-model.html');
-
-  precacheUrls.push('/posts/interview-jvm-GarbageCollection.html');
-
   precacheUrls.push('/posts/hmap.html');
 
-  precacheUrls.push('/posts/interview-jvm-classloader.html');
+  precacheUrls.push('/posts/interview-jvm-model.html');
 
   precacheUrls.push('/posts/interview-MySQL-transaction.html');
 
-  precacheUrls.push('/posts/rabbitmq.html');
+  precacheUrls.push('/posts/interview-jvm-GarbageCollection.html');
+
+  precacheUrls.push('/posts/interview-jvm-classloader.html');
 
   precacheUrls.push('/posts/interview-MySQL-common.html');
+
+  precacheUrls.push('/posts/interview-MySQL-optimization.html');
 
   precacheUrls.push('/posts/mysql-configure.html');
 
@@ -46,7 +46,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/redis-cluster.html');
 
-  precacheUrls.push('/posts/interview-MySQL-optimization.html');
+  precacheUrls.push('/posts/ribbon-load-balance.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
